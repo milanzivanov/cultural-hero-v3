9230f5abec50c8b7a386916d07e0e741b1f4ff81
milan <milanzivanov@users.noreply.github.com>
2025-09-30 22:48:39 +0200

    modification

--- src/components/AnimatedBorder.tsx
+++ src/components/AnimatedBorder.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 function AnimatedBorder({ timeout }: { timeout?: number }) {
   return (
     <motion.div
-      className="border-1 border-t border-gray-300 dark:border-gray-500 mb-6"
+      className="h-px border-t border-gray-300 dark:border-gray-500 mb-6"
       initial={{ width: 0 }}
       animate={{ width: "100%" }}
       transition={{ duration: timeout, ease: "easeOut" }}
