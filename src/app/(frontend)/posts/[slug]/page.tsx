6a57496c6c83750c5f1185a31c944d42ef2b562c
milan <milanzivanov@users.noreply.github.com>
2025-10-06 11:07:51 +0200

    generateMetadata

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -3,6 +3,22 @@ import { POST_BY_SLUG_QUERY } from "@/sanity/lib/queries";
 
 import { notFound } from "next/navigation";
 import { Post } from "@/components/Post";
+import type { Metadata } from "next";
+
+type Props = {
+  params: Promise<{ slug: string }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { data: posts } = await sanityFetch({
+    query: POST_BY_SLUG_QUERY,
+    params
+  });
+
+  return {
+    title: posts?.title ? `Kulturni heroj - ${posts.title}` : "Kulturni heroj"
+  };
+}
 
 export default async function Page({
   params
