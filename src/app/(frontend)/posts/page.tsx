467ae926c4a43092b7cc99b18aad8a8e7a3d0698
milan <milanzivanov@users.noreply.github.com>
2025-10-09 13:26:43 +0200

    search by title added

--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -1,9 +1,10 @@
 import { sanityFetch } from "@/sanity/lib/live";
 import { POSTS_QUERY } from "@/sanity/lib/queries";
-import { Title } from "@/components/Title";
 import { PostCard } from "@/components/PostCard";
 import BackToTopButton from "@/components/BackToTopButton";
 import { Header } from "@/components/Header";
+
+import Form from "next/form";
 
 export const metadata = {
   title: "Kulturni heroj | Naslovi"
@@ -16,9 +17,19 @@ export default async function Page() {
     <>
       <Header />
       <main className=" grid grid-cols-1 px-0 pb-12">
-        <div className="bg-slate-200 dark:bg-slate-700 pl-5 py-3 md:p-6">
+        <div className="bg-slate-200 dark:bg-slate-700 px-5 py-3 md:p-6">
           <div className="max-w-7xl mx-auto">
-            <Title>Naslovi</Title>
+            <Form
+              action="/search"
+              className="w-full sm:w-auto sm:flex-1 sm:mx-4 mt-2 sm:mt-0"
+            >
+              <input
+                type="text"
+                name="query"
+                placeholder="Pretražite naslove..."
+                className="w-full bg-gray-100 text-gray-800 px-4 py-4 rounded-xl focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 focus:outline-none border border-gray-300"
+              />
+            </Form>
           </div>
         </div>
         <div className="max-w-7xl p-5 md:p-0 mx-auto flex flex-col gap-10 py-6 md:py-12">
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,8 +2,8 @@ import { PropsWithChildren } from "react";
 
 export function Title(props: PropsWithChildren) {
   return (
-    <h1 className="text-xl md:text-2xl lg:text-4xl font-semibold text-slate-700 dark:text-slate-50 tracking-wide text-pretty max-w-3xl">
+    <h2 className="text-xl md:text-2xl lg:text-4xl font-semibold text-slate-700 dark:text-slate-50 tracking-wide text-pretty max-w-3xl">
       {props.children}
-    </h1>
+    </h2>
   );
 }
